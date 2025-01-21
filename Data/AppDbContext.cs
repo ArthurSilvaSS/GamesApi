b44905e5a23501a0de6a8a2eebbb6f8b07072d67
Arthur Silva <arthur.silva443@gmail.com>
2025-01-21 18:48:15 -0300

    Configuring Entity FrameWork Core, MySQL and Swagger in the program class

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace GamesAPI.Data
+﻿using GamesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+        public DbSet<Game> Games { get; set; }
     }
 }
