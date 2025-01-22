e2cda8960e944efda0a05654ed9a1b269e16c753
Arthur Silva <arthur.silva443@gmail.com>
2025-01-22 19:53:48 -0300

    Creation of the controller folder and the get games endpoint

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+﻿using GamesAPI.Data;
+using GamesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GamesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GamesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetAllGames()
+        {
+            try
+            {
+                var games = await _context.Games.ToListAsync();
+                return Ok(games);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+    }
+}
