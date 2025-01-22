1cb943a2748517ff601b34afdc57a0d1cd617760
Arthur Silva <arthur.silva443@gmail.com>
2025-01-22 19:13:13 -0300

    Creation of initial data for testing in the database

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,5 +7,24 @@ namespace GamesAPI.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
         public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Game>().HasData(
+                new Game 
+                { 
+                    Id = 1,
+                    Name = "Cyberpunk 2077",
+                    Description = "Description Cyberpunk 2077 ",
+                    imageUrl = "https://store-images.s-microsoft.com/image/apps.64394.13510798887568268.0b1b1b1b-0b1b-0b1b-0b1b-0b1b0b1b0b1b?w=672&h=378&q=80&mode=letterbox&background=%23FFE4E4E4&format=jpg",
+                    Genre = "RPG",
+                    Platform = "Steam",
+                    Publisher = "CD Projekt Red"
+                }
+                
+            );
+        }
     }
 }
