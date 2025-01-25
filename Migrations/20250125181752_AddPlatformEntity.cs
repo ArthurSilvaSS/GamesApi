4355274014a265f15a3b957df077d00097c04918
Arthur Silva <arthur.silva443@gmail.com>
2025-01-25 15:37:01 -0300

    Migration of created models and their relationships

---
+++ Migrations/20250125181752_AddPlatformEntity.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlatformEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.CreateTable(
+                name: "Plataforms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PlataformType = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plataforms", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "GamePlataforms",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "int", nullable: false),
+                    PlataformId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlataforms", x => new { x.GameId, x.PlataformId });
+                    table.ForeignKey(
+                        name: "FK_GamePlataforms_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlataforms_Plataforms_PlataformId",
+                        column: x => x.PlataformId,
+                        principalTable: "Plataforms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlataforms_PlataformId",
+                table: "GamePlataforms",
+                column: "PlataformId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePlataforms");
+
+            migrationBuilder.DropTable(
+                name: "Plataforms");
+
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "Description", "Genre", "Name", "Platform", "Publisher", "imageUrl" },
+                values: new object[] { 1, "Description Cyberpunk 2077 ", "RPG", "Cyberpunk 2077", "Steam", "CD Projekt Red", "https://store-images.s-microsoft.com/image/apps.64394.13510798887568268.0b1b1b1b-0b1b-0b1b-0b1b-0b1b0b1b0b1b?w=672&h=378&q=80&mode=letterbox&background=%23FFE4E4E4&format=jpg" });
+        }
+    }
+}
