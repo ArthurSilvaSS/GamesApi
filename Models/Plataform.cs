33d824aa1a3d13b7ddc1e7690727534b4d682f64
Arthur Silva <arthur.silva443@gmail.com>
2025-01-25 15:23:22 -0300

    Creating data model for plataform the games

---
+++ Models/Plataform.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesAPI.Models
+{
+    public class Plataform
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PlataformType { get; set; }
+
+        public ICollection<GamePlataform> GamePlataforms{ get; set; }
+    }
+}
