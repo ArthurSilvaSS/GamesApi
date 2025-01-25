89c257662332d3e1df1f86801087a772ef661499
Arthur Silva <arthur.silva443@gmail.com>
2025-01-25 15:27:10 -0300

    Creating data model to manage games and plataforms relationship

---
+++ Models/GamePlataform.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesAPI.Models
+{
+    public class GamePlataform
+    {
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        public int PlataformId { get; set; }
+        public Plataform Plataform { get; set; }
+    }
+}
