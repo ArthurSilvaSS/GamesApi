45e79ab29862d768526d87e177863bba11a56438
Arthur Silva <arthur.silva443@gmail.com>
2025-01-26 18:41:09 -0300

    Creation of DTOs

---
+++ DTOs/GameDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace GamesAPI.DTOs
+{
+    public class GameDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public List<int> PlatformIds { get; set; }
+    }
+}
---
+++ DTOs/GameDetailsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace GamesAPI.DTOs
+{
+    public class GameDetailsDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public List<PlatformDTO> Platforms { get; set; }
+    }
+}
