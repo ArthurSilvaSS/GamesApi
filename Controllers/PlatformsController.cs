c743be31ca20eee374ae6bb91f7bd42e96c60146
Arthur Silva <arthur.silva443@gmail.com>
2025-01-26 15:45:06 -0300

    Creation of the platform controller

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,17 @@
+﻿using GamesAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public PlatformsController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+
+}
