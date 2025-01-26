1d8d88b27f8a7e6571494dd1a8cb18bb22e3e166
Arthur Silva <arthur.silva443@gmail.com>
2025-01-26 19:06:06 -0300

    DTO file placed in the right path

---
+++ DTOs/CreatePlatformDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesAPI.DTOs
+{
+    public class CreatePlatformDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string PlatformType { get; set; }
+    }
+}
