71c10c6762f6e87b90eeced9c198b577c0bd02e8
Arthur Silva <arthur.silva443@gmail.com>
2025-01-26 15:51:14 -0300

    Added get and get id endpoints

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,5 +1,7 @@
 ﻿using GamesAPI.Data;
+using GamesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamesAPI.Controllers
 {
@@ -11,6 +13,36 @@ namespace GamesAPI.Controllers
         public PlatformsController(AppDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plataform>>> GetAllPlatforms()
+        {
+            try
+            {
+                var platforms = await _context.Plataforms.ToListAsync();
+                return Ok(platforms);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plataform>> GetPlatformById(int id)
+        {
+            try
+            {
+                var platform = await _context.Plataforms.FindAsync(id);
+                if (platform == null)
+                    return NotFound($"Platform with ID {id} was not found");
+                return Ok(platform);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
         }
     }
 
