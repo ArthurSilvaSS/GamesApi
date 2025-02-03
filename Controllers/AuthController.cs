d923eb84bc01e571f5b3cf1381367e0ee3b19cca
Arthur Silva <arthur.silva443@gmail.com>
2025-02-02 21:54:01 -0300

    Creation of the JWT controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly JwtService _jwtService;
+
+    public AuthController(JwtService jwtService)
+    {
+        _jwtService = jwtService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request.Email == "user@example.com" && request.Password == "pasword")
+        {
+            var token = _jwtService.GenerateToken("123", request.Email);
+            return Ok(new {Token = token});
+        }
+        return Unauthorized();
+    }
+}
+
+public class LoginRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
