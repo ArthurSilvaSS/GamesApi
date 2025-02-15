b7a2689a180db92d052f3525f59908734662f51a
Arthur Silva <arthur.silva443@gmail.com>
2025-02-15 14:41:41 -0300

    Implementation of user registration

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.Data;
+﻿using GamesAPI.DTOs.Authentication;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/[controller]")]
@@ -6,10 +7,30 @@ using Microsoft.AspNetCore.Mvc;
 public class AuthController : ControllerBase
 {
     private readonly JwtService _jwtService;
+    private readonly IUserService _userService;
 
-    public AuthController(JwtService jwtService)
+    public AuthController(JwtService jwtService, IUserService userService)
     {
         _jwtService = jwtService;
+        _userService = userService;
+    }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserDto request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        try
+        {
+            var user = await _userService.RegisterUserAsync(request);
+            // Opcionalmente, gere um token logo após o registro
+            var token = _jwtService.GenerateToken(user.Id.ToString(), user.Email);
+            return Ok(new { Message = "Usuário criado com sucesso!", Token = token });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { Message = ex.Message });
+        }
     }
 
     [HttpPost("login")]
